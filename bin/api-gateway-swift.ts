89e493d951980205a39fd72aa58c20af1e17d88e
Hiroki Kato <mail@cockscomb.info>
2020-06-14 14:11:56 +0900

    Set environment

--- bin/api-gateway-swift.ts
+++ bin/api-gateway-swift.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { ApiGatewaySwiftStack } from '../lib/api-gateway-swift-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { ApiGatewaySwiftStack } from "../lib/api-gateway-swift-stack";
 
 const app = new cdk.App();
-new ApiGatewaySwiftStack(app, 'ApiGatewaySwiftStack');
+new ApiGatewaySwiftStack(app, "ApiGatewaySwiftStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
